4094ef379a68be03f2b6b6f0a768a14c15689a6d
tackelua <tackelua@gmail.com>
2018-10-04 14:38:05 +0700

    scheduled commit

--- scheduled-commit.bat
+++ scheduled-commit.bat
@@ -1,8 +1,19 @@
-@echo START TIME: %time%
+echo
+echo START TIME: %time%
+set /A STARTTIME=(1%STARTTIME:~0,2%-100)*360000 + (1%STARTTIME:~3,2%-100)*6000 + (1%STARTTIME:~6,2%-100)*100 + (1%STARTTIME:~9,2%-100)
+
 cd C:\Users\tacke\Desktop\test auto commit
 git checkout backup
 git add .
 git commit -m "scheduled commit"
 git push
-@echo END TIME: %time%
+
+set /A ENDTIME=(1%ENDTIME:~0,2%-100)*360000 + (1%ENDTIME:~3,2%-100)*6000 + (1%ENDTIME:~6,2%-100)*100 + (1%ENDTIME:~9,2%-100)
+set /A DURATIONH=%DURATION% / 360000
+set /A DURATIONM=(%DURATION% - %DURATIONH%*360000) / 6000
+set /A DURATIONS=(%DURATION% - %DURATIONH%*360000 - %DURATIONM%*6000) / 100
+set /A DURATIONHS=(%DURATION% - %DURATIONH%*360000 - %DURATIONM%*6000 - %DURATIONS%*100)
+echo
+echo END TIME: %time%
+echo DURATIONHS: %DURATIONHS%
 pause
