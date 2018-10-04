6388b8c80a42c5aee617d8a4862c4cea9b9ff6fd
tackelua <tackelua@gmail.com>
2018-10-04 14:50:16 +0700

    scheduled commit

--- scheduled-commit.bat
+++ scheduled-commit.bat
@@ -1,16 +1,22 @@
 @echo.
 @echo START TIME: %time%
 
+@echo.
 @echo ^>^> cd C:\Users\tacke\Desktop\test auto commit
 @cd C:\Users\tacke\Desktop\test auto commit
+
+@echo.
 @echo ^>^> git checkout backup
 @git checkout backup
+
+@echo.
 @echo ^>^> git add .
 @git add .
+
 @echo.
 @echo ^>^> git commit -m "scheduled commit"
 @git commit -m "scheduled commit"
-@git commit -m "scheduled commit"
+
 @echo.
 @echo ^>^>git push
 @git push
